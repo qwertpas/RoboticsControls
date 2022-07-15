80c75ae99499e59bbcfcefdbba002ca5437c843c
Ashray._.g <ashray.gupta@gmail.com>
2022-07-15 12:24:54 -0700

    kalman

---
+++ StateSpaceControl/kalmanFilter/singleValueStateUpdateEquation.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rnd
+
+realValue = 2.0
+maxVariance = 0.8
+xnn1 = 0
+xnn = 0
+
+value = np.array([])
+measures = np.array([])
+filtered = np.array([])
+t = np.array([])
+
+for i in range(1, 100):
+    measured = realValue + (rnd.random()*2 - 1) * maxVariance
+
+    xnn = xnn1 + (1.0/i) * (measured - xnn1)
+    xnn1 = xnn
+
+    value = np.append(value, realValue)
+    measures = np.append(measures, measured)
+    filtered = np.append(filtered, xnn)
+    t = np.append(t, i)
+
+    plt.plot(t, value, marker = "o", markerfacecolor="r")
+    plt.plot(t, measures, marker = "o", markerfacecolor="b")
+    plt.plot(t, filtered, marker = "o", markerfacecolor="g")
+    plt.draw()
+    plt.pause(0.03)
+
+plt.show()
