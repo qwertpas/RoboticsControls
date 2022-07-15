4074f3a131b93725d656e1a17a55a0d868123600
Ashray._.g <ashray.gupta@gmail.com>
2022-07-15 13:24:16 -0700

    updates

--- StateSpaceControl/kalmanFilter/linearValueGHFilter.py
+++ StateSpaceControl/kalmanFilter/linearValueGHFilter.py
@@ -2,26 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rnd
 
-pos = 0
-velo = 2
+pos = 10
+velo = 4
 
-maxVariance = 2
+maxVariance = 4
 
 alpha = 0.2
 beta = 0.1
 
-xnn1 = 0
+xnn1 = 14
 xnn = 0
 
 xnn_dot = 0
-xnn1_dot = 0
+xnn1_dot = 4
 
 value = np.array([]) #position real
 measures = np.array([]) #measure distance
 filtered = np.array([]) #estimated distance
 t = np.array([]) #time
 
-for i in range(1, 30):
+for i in range(1, 10):
     pos = pos + velo
     measured = pos + (rnd.random()*2 -1)*maxVariance
 
