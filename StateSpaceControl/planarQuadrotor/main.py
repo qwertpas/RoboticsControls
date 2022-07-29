6f8ce2e9e506acd054ea5d8803f56b84124142bd
Christopher Xu <cyx3@illinois.edu>
2022-07-29 00:22:51 -0700

    tilted but no spin thrust

--- StateSpaceControl/planarQuadrotor/main.py
+++ StateSpaceControl/planarQuadrotor/main.py
@@ -8,13 +8,18 @@ xn = ssm.x
 state = np.array([[0], [0], [-np.pi/4], [0], [0], [0]])
 
 X, Y, P = [ssm.x[0, 0]], [ssm.x[1, 0]], [ssm.x[2, 0]]
+X_dots, Y_dots, P_dots = [], [], []
 
-for i in range(1, round(3/0.01)):
+for i in range(1, 100):
     xn = sse.step(xn, ssm.u)
     state = state + (xn * ssm.dt)
     X = np.append(X, state[0, 0])
     Y = np.append(Y, state[1, 0])
     P = np.append(P, state[2, 0])
+
+    X_dots.append(state[3, 0])
+    Y_dots.append(state[4, 0])
+    P_dots.append(state[5, 0])
 
     # plt.plot(X, Y, marker="o", markerfacecolor="r")
 
@@ -26,10 +31,12 @@ for i in range(1, round(3/0.01)):
 plt.scatter(X, Y)
 plt.quiver(X, Y, -np.sin(P), np.cos(P), alpha=0.5)
 
-fig, axs = plt.subplots(2, 1, sharex=True)
+fig, axs = plt.subplots(3, 1, sharex=True)
 axs[0].plot(X, label='x')
 axs[0].plot(Y, label='y')
 axs[1].plot(P/(2*np.pi), label='rotation')
+axs[2].plot(X_dots, label='x_dot')
+axs[2].plot(Y_dots, label='y_dot')
 for ax in axs:
     ax.legend()
 
--- StateSpaceControl/planarQuadrotor/stateSpaceModel.py
+++ StateSpaceControl/planarQuadrotor/stateSpaceModel.py
@@ -5,13 +5,13 @@ M = 1 #Mass in kg
 L = 0.3 #Span
 I = L*L * M #MOI pretending rod
 
-dt = 0.01 #timestep
+dt = 0.02 #timestep
 
 #state = [x, y, phi, x', y', phi']
 x = np.array([[0], [0.0], [-np.pi/4], [0], [0], [0]])
 
 #input = [u1, u2, g]
-u = np.array([[50], [1], [9.8]])
+u = np.array([[50], [0], [9.8]])
 #u1 = F1 + F2 = thrust total
 #u2 = (L/2) * (F1 - F2) = moment 
 
